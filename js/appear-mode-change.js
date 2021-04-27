324eb09619d774e0e550b6a72adda5956e3becd2
Dong Hyeon Kim <across0406@nate.com>
2021-04-27 16:15:10 +0900

    Hello World! on js-intro.html file.
    - Put some javascript codes on js-intro.html.

--- js/appear-mode-change.js
+++ js/appear-mode-change.js
@@ -5,61 +5,11 @@ function judgePreferDark() {
         )
         .matches;
 
-    if (prefersDark) {
-        document
-            .querySelector('body')
-            .style
-            .backgroundColor = 'black';
-        document
-            .querySelector('body')
-            .style
-            .color = 'white';
-        document
-            .querySelector(
-                "#main-appearance.appear-mode-switch input[type=checkbox]"
-            )
-            .checked = true;
-    } else {
-        document
-            .querySelector('body')
-            .style
-            .backgroundColor = 'white';
-        document
-            .querySelector('body')
-            .style
-            .color = 'black';
-        document
-            .querySelector(
-                "#main-appearance.appear-mode-switch input[type=checkbox]"
-            )
-            .checked = false;
-    }
+    return prefersDark;
 }
 
-$(document).ready(judgePreferDark());
-
-function changePreferMode(element) {
-    var checked = document
-        .querySelector(
-            "#main-appearance.appear-mode-switch input[type=checkbox]"
-        )
-        .checked;
-
-    if (checked) {
-        document
-            .querySelector('body')
-            .style
-            .backgroundColor = 'white';
-        document
-            .querySelector('body')
-            .style
-            .color = 'black';
-        document
-            .querySelector(
-                "#main-appearance.appear-mode-switch input[type=checkbox]"
-            )
-            .checked = false;
-    } else {
+function initPreferMode(isNight) {
+    if (isNight) {
         document
             .querySelector('body')
             .style
@@ -69,9 +19,49 @@ function changePreferMode(element) {
             .style
             .color = 'white';
         document
-            .querySelector(
-                "#main-appearance.appear-mode-switch input[type=checkbox]"
-            )
-            .checked = true;
+            .getElementById('mode-toggle-slider')
+            .style
+            .translate = '26px';
+    } else {
+        document
+            .querySelector('body')
+            .style
+            .backgroundColor = 'white';
+        document
+            .querySelector('body')
+            .style
+            .color = 'black';
+        document
+            .getElementById('mode-toggle-slider')
+            .style
+            .translate = '26px';
+    }
+
+    document
+            .getElementById('mode-toggle')
+            .style
+            .checked = isNight;    
+}
+
+function changePreferMode(element) {
+    if (!element.checked) {
+        document
+            .querySelector('body')
+            .style
+            .backgroundColor = 'white';
+        document
+            .querySelector('body')
+            .style
+            .color = 'black';
+
+    } else {
+        document
+            .querySelector('body')
+            .style
+            .backgroundColor = 'black';
+        document
+            .querySelector('body')
+            .style
+            .color = 'white';
     }
 }
